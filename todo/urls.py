9ac409efdcb85130a6a1ef9a57ec68c1467b9a54
Matthew Kelly <mkonabike@gmail.com>
2018-08-03 15:50:03 -0500

    todo app in django

---
+++ todo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+path('', views.index, name='index'),
+path('add', views.addToDo, name='add'),
+path('complete/<todo_id>', views.completeToDo, name='complete'),
+path('deletecomplete', views.deleteCompleted, name='deletecomplete')
+]
---
+++ todo/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from .models import ToDo
+from .forms import ToDoForm
+from django.views.decorators.http import require_POST
+
+
+def index(request):
+	todo_list = ToDo.objects.order_by('id')
+	form = ToDoForm()
+	context = {'todo_list' : todo_list, 'form' : form}
+	return render(request, 'todo/index.html', context)
+
+@require_POST
+def addToDo(request):
+	form = ToDoForm(request.POST)
+	print(request.POST['text'])
+
+	if form.is_valid():
+		new_todo = ToDo(text=request.POST['text'])
+		new_todo.save()
+	return redirect('index')
+
+def completeToDo(request, todo_id):
+	todo = ToDo.objects.get(pk=todo_id)
+	todo.complete = True
+	todo.save()
+
+	return redirect('index')
+
+def deleteCompleted(request):
+	ToDo.objects.filter(complete_exact=True).delete()
+
+	return redirect('index')
